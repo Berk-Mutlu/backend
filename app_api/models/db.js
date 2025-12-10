a26747d091c88315adf2d997176267ec3fc4bef5
Berk Mutlu <iam.berkmutlu@gmail.com>
2025-12-10 14:59:50 +0300

    Update
    New Week Update

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,24 +1,19 @@
-var mongoose=require('mongoose');
-var dbURI='mongodb+srv://iamberkmutlu_db_user:z5Owtl03J0r9ahN3@cluster0.kywkdli.mongodb.net/?appName=Cluster0';
+//var dbURI='mongodb+srv://iamberkmutlu_db_user:z5Owtl03J0r9ahN3@cluster0.kywkdli.mongodb.net/?appName=Cluster0';var mongoose=require('mongoose');
+var mongoose=require("mongoose");
+var dbURI="mongodb://localhost/mekanbul";
 mongoose.connect(dbURI);
-
-mongoose.connection.on("cnnected",function(){
-    console.log("Mongoose "+dbURI+" bağlantısı başarılı");
+mongoose.connection.on("connected",function(){
+    console.log("Mongoose "+dbURI+" adresindeki veritabanına bağlandı.");
 });
-
 mongoose.connection.on("error",function(){
-    console.log("Mongoose "+dbURI+" bağlantısı başarısız");
+    console.log("Mongoose bağlantı hatası.");
 });
-
 mongoose.connection.on("disconnected",function(){
-    console.log("Mongoose "+dbURI+" bağlantısı kesildi");
+    console.log("Mongoose bağlantısı kesildi.");
 });
-
-
-process.on('SIGINT',function(){
+process.on("SIGINT",function(){
     mongoose.connection.close();
-    console.log("Mongoose bağlantısı kapatıldı");
+    console.log("Mongoose uygulama sonlandırma nedeniyle bağlantıyı kapattı.");
     process.exit(0);
-    
 });
-require('./venue');
+require("./venue");
